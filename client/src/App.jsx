77949f0da16c0291add8cf7a6a96f687ab7f44b6
Dismalness <domainexpansion@protonmail.com>
2021-05-16 17:03:17 +0100

    second commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.scss';
+  import NavBar from './Components/NavBar'
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../styles/NavBar.scss'
+const NavBar = () => {
+    return (
+        <div class="NavBar">
+            <h1 class="logo">
+                atatatta
+            </h1>
+            <button>
+                Sign up
+            </button>
+            {/* <button >
+                {username}
+            </button> */}
+        </div>
+    );
+}
+
+export default NavBar;
