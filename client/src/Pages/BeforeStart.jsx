72bfe0fbefd500112245acce576efcc6eff4c229
dali <dalissj1@gmail.com>
2021-09-05 15:52:01 +0100

    finding other player through game id

--- client/src/Pages/BeforeStart.jsx
+++ client/src/Pages/BeforeStart.jsx
@@ -5,33 +5,47 @@ import { rdb } from '../firebase';
 import RdbProvider from '../contexts/RealTimeContext';
 import HomeButton from '../components/HomeButton';
 import '../styles/before-start.scss'
+import { useQuote } from '../customHooks';
 
 export default function BeforeStart() {
 
     const [foundPlayer, setFoundPlayer] = useState(false);
     const [gameCode, setGameCode] = useState("");
+    const {data} = useQuote ();
 
     //creating new game
     useEffect(()=>{
-        
+        if(!data) return ;
 
         const gameRef= rdb.ref("Game");
+        
         const game = {
+            
             player1: 0,
             player2: 0,
-            foundPlayer: false
+            foundPlayer: false,
+            data: data , 
+
         }
         setGameCode(gameRef.push(game).key);
-    },[])
+    },[data])
 
     useEffect(()=>{
+        if(!gameCode) return ;
 
-        //await finding player
-
-        // setTimeout(()=>{
-        //     setFoundPlayer(true)
-        // },2000)
-    },[])
+        const gameRef = rdb.ref("Game");
+        gameRef.on("value", snapshot=>{
+            const games = snapshot.val();
+            
+            for(let id in games){
+                console.log( gameCode,id)
+                console.log(gameCode==id)
+                if(id != gameCode) continue ; 
+                
+                setFoundPlayer(games[id].foundPlayer);
+            }
+        })
+    },[gameCode])
 
 
     const value = {
@@ -41,6 +55,7 @@ export default function BeforeStart() {
     return (
         <RdbProvider value={value}>
             <div className="before-start">
+               
                 <HomeButton/>
             {
                 foundPlayer?(
--- client/src/Pages/JoinPage.jsx
+++ client/src/Pages/JoinPage.jsx
@@ -1,27 +1,49 @@
-import React, { useRef } from 'react'
+import React, { useRef,useState } from 'react'
 import HomeButton from '../components/HomeButton'
+import { rdb } from '../firebase';
 import '../styles/join.scss'
+import StartPage from './StartPage';
 
 
 export default function JoinPage() {
 
+    const [error, setError] = useState("");
+    const [foundGame, setFoundGame] = useState(false)
     const inputRef = useRef();
 
     const handleJoin = ()=>{
-        
+        const {value} = inputRef.current;
+
+        const GameRef= rdb.ref('Game').child(value) ; 
+        if(!GameRef){
+            // display error message
+            setError("Game not found !")
+            return ;
+        }
+        GameRef.update({
+            foundPlayer: true
+        })
+        setFoundGame(true);
     }
 
     return (
         <div className="join">
             <HomeButton/>
-            <div>
-                <label htmlFor="code-input">Enter game code</label>
-                <div className="input-place">
-                    <input type="text" id="code-input" placeholder="enter code here"  ref={inputRef}/>
-                    <div className="line"></div>
+            {
+                foundGame? (
+                    <StartPage/>
+                ):
+                <div>
+                    <label htmlFor="code-input">Enter game code</label>
+                    <div className="input-place">
+                        <input type="text" id="code-input" placeholder="enter code here"  ref={inputRef}/>
+                        <div className="line"></div>
+                    </div>
+                    <button onClick={handleJoin}>Join Game</button>
+                    <span className="error">{error}</span>
                 </div>
-                <button onClick={handleJoin}>Join Game</button>
-            </div>
+            }
+            
         </div>
     )
 }
