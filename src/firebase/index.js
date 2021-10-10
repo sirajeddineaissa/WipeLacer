b522a7e2421f6f505867f5d2bc170ae54da95f29
Dismalness <domainexpansion@protonmail.com>
2021-10-10 12:02:15 +0100

    fix

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,15 +2,14 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
-const app = firebase.initializeApp({
+export default app = firebase.initializeApp({
   apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.SENDER_ID,
-  appId: process.env.APP_ID
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 });
 
 export const auth = app.auth();
 export const db = app.firestore();
-export default app;
